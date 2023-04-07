f7367f170e26db6366f5d69a1499b601e2399c2d
hsk-kr <hsk.coder@gmail.com>
2023-04-07 22:57:59 +0200

    add packages for using tailwind with storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,22 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../components/**/*.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-styling",
+      options: {
+        postCss: true,
+      },
+    },
+    "storybook-addon-next",
   ],
   framework: {
     name: "@storybook/nextjs",
