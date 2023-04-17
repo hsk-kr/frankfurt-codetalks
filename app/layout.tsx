2f31e37455a8a159b4788580785af5b8415c465e
hsk-kr <hsk.coder@gmail.com>
2023-04-17 21:30:40 +0200

    update metadata for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,32 @@
-import "./globals.css";
-import { Roboto } from "next/font/google";
-import Header from "@/components/common/Header";
+import './globals.css';
+import { Roboto } from 'next/font/google';
+import Header from '@/components/common/Header';
 
 const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["100", "300", "400", "500", "700", "900"],
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
 });
 
+const title = 'Frankfurt CodeTalks';
+const description =
+  'Join our programming Meetup if you live near Frankfurt and are interested in coding.';
+
 export const metadata = {
-  title: "Frankfurt CodeTalks",
-  description:
-    "Join our programming Meetup if you live near Frankfurt and are interested in coding.",
+  title,
+  description,
+  keywords:
+    'Frankfurt Programming, Frankfurt Developer, Frankfurt Korean, 프랑크푸르트 프로그래밍, 프랑크푸르트 개발, 프랑크푸르트 스터디, 프랑크푸르트 한국',
+  openGraph: {
+    title,
+    type: 'website',
+    description,
+    siteName: title,
+    url: 'https://fct.h-code.dev/',
+  },
+  twitter: {
+    title,
+    description,
+  },
 };
 
 export default function RootLayout({
